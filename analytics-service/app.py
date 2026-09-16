fb190f1b14781f5eb8f08abf00d55ab3e695fb61
Leonardo <leonardomartins0700@gmail.com>
2026-09-15 22:52:18 -0300

    test: demonstra ciclo completo de GitOps ao vivo

--- analytics-service/app.py
+++ analytics-service/app.py
@@ -148,4 +148,5 @@ start_worker()
 if __name__ == '__main__':
     port = int(os.getenv("PORT", 8005))
     app.run(host='0.0.0.0', port=port, debug=False)
-// Demonstração GitOps - ter 15 set 2026 22:42:16 -03
+# Demonstração GitOps - ter 15 set 2026 22:49:10 -03
+# Demonstração GitOps - ter 15 set 2026 22:52:18 -03
